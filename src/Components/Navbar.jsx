3c9024f41021d020a9bab46ead6a1e0df4f28777
Shubham Kumar Singh <102406929+ShubhamGet@users.noreply.github.com>
2023-02-10 08:05:11 +0530

    firts

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
                     <div className='col-10 mx-auto'>
                         <nav className="navbar navbar-expand-lg bg-body-light">
                             <div className="container-fluid">
-                                <Link className="navbar-brand" to="/">"Shubham Singh"</Link>
+                                <h1 className="navbar-brand">"Shubham Singh"</h1>
                                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                     <span className="navbar-toggler-icon"></span>
                                 </button>
